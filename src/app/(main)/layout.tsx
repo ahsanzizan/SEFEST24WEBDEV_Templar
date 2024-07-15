2c648a12f92b1a78e0b364353ca1b1ebd479d6fa
FadhilKholaf <fadhilgd123@gmail.com>
2024-07-15 21:42:23 +0700

    feat: add landing page

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar';
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import Home from './parts/Home';
+
+export default function page() {
+  return (
+    <main className="bg-[#F2F2F2]">
+      <Home />
+    </main>
+  );
+}
