6fed988a0bc2832f91b1cd75ec4b6a6c7f83aea6
FadhilKholaf <fadhilgd123@gmail.com>
2024-07-19 07:32:37 +0700

    update: remove debuger

--- src/app/(main)/parts/Organization.tsx
+++ src/app/(main)/parts/Organization.tsx
@@ -11,8 +11,6 @@ const data: string[] = [
 export default function Organization() {
   const organizations = data.slice(0, 4);
 
-  console.log(organizations);
-
   return (
     <section className="min-h-screen w-screen px-20">
       <div className="flex flex-col gap-y-4">
