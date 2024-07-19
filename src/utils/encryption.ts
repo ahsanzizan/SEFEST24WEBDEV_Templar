4d092c622b570ee3e72931f681e230fc5ae56f6e
panntod <pandhu.munjalindra@gmail.com>
2024-07-20 05:59:19 +0700

    feat: Add encryption utility functions using bcrypt

---
+++ src/utils/encryption.ts
@@ -0,0 +1,13 @@
+import { hashSync, compareSync } from "bcrypt";
+
+const BCRYPT_ROUNDS = 10;
+
+export const encrypt = (plainText: string): string => {
+  const hashed = hashSync(plainText, BCRYPT_ROUNDS);
+  return hashed;
+};
+
+export const compareHash = (plainText: string, cipherText: string): boolean => {
+  const compare = compareSync(plainText, cipherText);
+  return compare;
+};
